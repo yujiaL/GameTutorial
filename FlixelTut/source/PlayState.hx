f03645aa7547bae59e4475bcad104428516b4671
Fengjun Dong <jundong321@gmail.com>
2017-04-27 21:37:39 -0700

    Finished Step 7
    Added animation.
    Added tilemap and zoom.

--- FlixelTut/source/PlayState.hx
+++ FlixelTut/source/PlayState.hx
@@ -1,22 +1,54 @@
 package;
 
 import flixel.FlxState;
+import flixel.FlxObject;
+import flixel.FlxG;
+import flixel.addons.editors.ogmo.FlxOgmoLoader;
 import flixel.text.FlxText;
+import flixel.tile.FlxTilemap;
 
 class PlayState extends FlxState
 {
 	
 	private var _player:Player;
+	private var _map:FlxOgmoLoader;
+	private var _mWalls:FlxTilemap;
 	
 	override public function create():Void
 	{
-		_player = new Player(20, 20);
+		// Set up tile.
+		_map = new FlxOgmoLoader(AssetPaths.room_001__oel);
+		_mWalls = _map.loadTilemap(AssetPaths.tiles__png, 16, 16, "walls");
+		_mWalls.follow();
+		_mWalls.setTileProperties(1, FlxObject.NONE);
+		_mWalls.setTileProperties(2, FlxObject.ANY);
+		add(_mWalls);
+		
+		// Set up player.
+		_player = new Player();
+		_map.loadEntities(placeEntities, "entities");
 		add(_player);
+		
+		// Set camera.
+		FlxG.camera.follow(_player, TOPDOWN, 1);
+		
 		super.create();
+	}
+	
+	private function placeEntities(entityName:String, entityData:Xml):Void
+	{
+		var x:Int = Std.parseInt(entityData.get("x"));
+		var y:Int = Std.parseInt(entityData.get("y"));
+		if (entityName == "player")
+		{
+			_player.x = x;
+			_player.y = y;
+		}
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		FlxG.collide(_player, _mWalls);
 	}
 }
--- FlixelTut/source/Player.hx
+++ FlixelTut/source/Player.hx
@@ -2,6 +2,7 @@ package;
 
 import flixel.FlxSprite;
 import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.system.FlxAssets.FlxGraphicAsset;
 import flixel.util.FlxColor;
 import flixel.math.FlxPoint;
@@ -13,8 +14,27 @@ class Player extends FlxSprite
 	public function new(?X:Float=0, ?Y:Float=0) 
 	{
 		super(X, Y);
-		makeGraphic(16, 16, FlxColor.BLUE);
+		
+		// Make the actual size of the player smaller.
+		setSize(8, 14);
+		offset.set(0, 2);
+		
+		// Load animation for the player.
+		loadGraphic(AssetPaths.player__png, true, 16, 16);
+		setFacingFlip(FlxObject.LEFT, false, false);
+		setFacingFlip(FlxObject.RIGHT, true, false);
+		animation.add("lr", [3, 4, 3, 5], 6, false);
+		animation.add("u", [6, 7, 6, 8], 6, false);
+		animation.add("d", [0, 1, 0, 2], 6, false);
+		
+		// Drag force to stop player from keeping walking.
 		drag.x = drag.y = 1600;
+	}
+	
+	override public function update(elapsed:Float):Void
+	{
+		movement();
+		super.update(elapsed);
 	}
 	
 	private function movement():Void
@@ -44,6 +64,7 @@ class Player extends FlxSprite
 					mA -= 45;
 				else if (_right)
 					mA += 45;
+				facing = FlxObject.UP;
 			}
 			else if (_down)
 			{
@@ -52,20 +73,35 @@ class Player extends FlxSprite
 					mA += 45;
 				else if (_right)
 					mA -= 45;
+				facing = FlxObject.DOWN;
 			}
 			else if (_left)
+			{
 				mA = 180;
+				facing = FlxObject.LEFT;
+			}
 			else if (_right)
+			{
 				mA = 0;
+				facing = FlxObject.RIGHT;
+			}
 			
 			velocity.set(speed, 0);
 			velocity.rotate(FlxPoint.weak(0, 0), mA);
+			
+			// Player the right animation for each direction.
+			if ((velocity.x != 0 || velocity.y != 0) && touching == FlxObject.NONE) 
+			{
+				switch (facing)
+				{
+					case FlxObject.LEFT, FlxObject.RIGHT:
+						animation.play("lr");
+					case FlxObject.UP:
+						animation.play("u");
+					case FlxObject.DOWN:
+						animation.play("d");
+				}
+			}
 		}
-	}
-	
-	override public function update(elapsed:Float):Void
-	{
-		movement();
-		super.update(elapsed);
 	}
 }
